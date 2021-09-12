6b13252c9d0deaa93f0784db8415fe1b64bc9c77
Priyadarshan Singh <singhpd75@gmail.com>
2021-09-12 18:30:38 +0530

    fix: frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config ={
     production: {
-        BACKEND: 'http://localhost:9000/',
+        BACKEND: 'http://localhost:9000/backend',
     }, 
     development: {
         BACKEND: 'http://localhost:9000/',
